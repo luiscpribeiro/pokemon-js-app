a2a1d233133ca6679c08429de48f84666373621d
luiscpribeiro <lcrgambelas@gmail.com>
2021-06-02 12:02:07 +0100

    Update scripts.js
    added new pokemons
    added conditional to point out bigger then 1.7m

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
 let pokemonList = [
   {name:'Bulbasaur', type:['grass','poison'], height:0.7},
+  {name:'Ivysaur', type:['grass', 'poison'], height:1},
+  {name:'Venusaur', type:['grass', 'poison'], height:2},
   {name:'Charmander', type:['fire'], height:0.6},
-  {name:'Squirtle', type:['water'], height:0.5}
+  {name:'Charmeleon', type:['fire'], height:1.1},
+  {name:'Charizard', type:['fire', 'flying'], height:1.7},
+  {name:'Squirtle', type:['water'], height:0.5},
+  {name:'Wartortle', type:['water'], height:1},
+  {name:'Blastoise', type:['water'], height:1.6}
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
-  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") ");
+  if (pokemonList[i].height > 1.7) {
+    document.write(pokemonList[i].name + " <strong>is huge!</strong> (height: " + pokemonList[i].height + ")<br>");
+  } else {
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")<br>");
+  }
 }
