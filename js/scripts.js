ddef8a96d2f236199c56c3b509f4bcb7fcd8fc99
Luis Ribeiro <81969714+luiscpribeiro@users.noreply.github.com>
2021-10-18 23:12:43 +0100

    IIFE and forEach changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,37 @@
-let pokemonList = [
-  {name:'Bulbasaur', type:['grass','poison'], height:0.7},
-  {name:'Ivysaur', type:['grass', 'poison'], height:1},
-  {name:'Venusaur', type:['grass', 'poison'], height:2},
-  {name:'Charmander', type:['fire'], height:0.6},
-  {name:'Charmeleon', type:['fire'], height:1.1},
-  {name:'Charizard', type:['fire', 'flying'], height:1.7},
-  {name:'Squirtle', type:['water'], height:0.5},
-  {name:'Wartortle', type:['water'], height:1},
-  {name:'Blastoise', type:['water'], height:1.6}
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name:'Bulbasaur', type:['grass','poison'], height:0.7},
+    {name:'Ivysaur', type:['grass', 'poison'], height:1},
+    {name:'Venusaur', type:['grass', 'poison'], height:2},
+    {name:'Charmander', type:['fire'], height:0.6},
+    {name:'Charmeleon', type:['fire'], height:1.1},
+    {name:'Charizard', type:['fire', 'flying'], height:1.7},
+    {name:'Squirtle', type:['water'], height:0.5},
+    {name:'Wartortle', type:['water'], height:1},
+    {name:'Blastoise', type:['water'], height:1.6}
+  ];
 
-function printPokemonList(){
-  for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1.7) {
-      document.write("<p>" + pokemonList[i].name + " <strong>is huge!</strong> (height: " + pokemonList[i].height + ")</p>");
-    } else {
-      document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")</p>");
-    }
+  function add(item) {
+    pokemonList.push(item);
   }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+function printPokemon(pokemon) {
+    if (pokemon.height >= 1.7) {
+      document.write("<p>" + pokemon.name + " is <strong>huge</strong>! (height: " + pokemon.height + ")</p>");
+    } else {
+      document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ")</p>");
+    }
 }
 
-printPokemonList();
+pokemonRepository.add({name: 'Pikachu', type:['Eletric'], height:0.4});
+pokemonRepository.getAll().forEach(printPokemon);
